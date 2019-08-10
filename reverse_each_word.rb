1a9c73405944707cc822b48d9d77465011ababef
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-10 04:45:30 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word( string)
+  split_string = string.split(" ")
+  split_string.each do |split_string|
+    word = split_string[index].split("")
+    word.length.times{ |index|
+      word = split_string[index].split("")
+      reversed = []
+      word.size.times { reversed << word.pop }
+      reversed.join
+    }
+  end
+end
