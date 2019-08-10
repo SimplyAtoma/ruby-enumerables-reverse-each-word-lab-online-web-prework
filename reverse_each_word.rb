56b26681f26130c5a208819abd3131ee082ec754
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-10 04:46:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,13 @@
 def reverse_each_word( string)
   split_string = string.split(" ")
   split_string.each do |split_string|
-    word = split_string[index].split("")
+    word = split_string.split("")
     word.length.times{ |index|
       word = split_string[index].split("")
       reversed = []
       word.size.times { reversed << word.pop }
       reversed.join
     }
+    reversed = split_string[index]
   end
 end
